f89ab0e5f1795ab3057bc5dedd32d88ac96f028e
Simrandeep <simrandeep.sappal@gmail.com>
2021-02-18 12:29:09 -0600

    added ScheduleActivity.java and activity_schedule.xml files

---
+++ app/src/main/java/com/example/coursescheduler/myGIU/ScheduleActivity.java
@@ -0,0 +1,15 @@
+package com.example.coursescheduler.myGIU;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.coursescheduler.R;
+
+public class ScheduleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_schedule);
+    }
+}
