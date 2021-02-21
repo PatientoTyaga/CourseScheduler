91e2f63cd0d9d92f02b3e6900d57f1d06e561055
Simrandeep <simrandeep.sappal@gmail.com>
2021-02-20 21:16:42 -0600

    made changed to MainActivity.java

--- app/src/main/java/com/example/coursescheduler/myGIU/MainActivity.java
+++ app/src/main/java/com/example/coursescheduler/myGIU/MainActivity.java
@@ -16,6 +16,7 @@ import com.example.coursescheduler.R;
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
     private List<Student> studentList;
+    private List<String> studentName = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,24 +24,16 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         setContentView(R.layout.activity_main);
         Spinner dropdown = findViewById(R.id.spinnerStudentName);
         String[][] database = Database.studentInfo.clone();
-        Student s1 = new Student(database[0][1], database[0][0]);
-        Student s2 = new Student(database[1][1], database[1][0]);
-        Student s3 = new Student(database[2][1], database[2][0]);
         Button submit = (Button)findViewById(R.id.buttonSchedules);
-        try {
 
-            studentList = new ArrayList<>();
-            studentList.add(s1);
-            studentList.add(s2);
-            studentList.add(s3);
-            String[] items = new String[]{s1.getStudentName(), s2.getStudentName(), s3.getStudentName()};
-            ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
-            dropdown.setAdapter(adapter);
-
-        } catch (final Exception e) {
-            System.out.println(e);
+        for(int i=0; i<database.length; i++){
+            Student s = new Student(database[i][1], database[i][0]);
+            studentName.add(s.getStudentName());
         }
 
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, studentName);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        dropdown.setAdapter(dataAdapter);
 
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
