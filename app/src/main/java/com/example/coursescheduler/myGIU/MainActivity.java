edea84faaa84aeadaa17eb7fad102a1f58f0be5b
Simrandeep <simrandeep.sappal@gmail.com>
2021-02-20 18:49:11 -0600

    Files updates: MainActivity.java and activity.xml

--- app/src/main/java/com/example/coursescheduler/myGIU/MainActivity.java
+++ app/src/main/java/com/example/coursescheduler/myGIU/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Spinner dropdown = findViewById(R.id.spinner1);
+        Spinner dropdown = findViewById(R.id.spinnerStudentName);
 
 
         try {
