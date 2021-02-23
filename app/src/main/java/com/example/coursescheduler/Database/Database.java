c8873eb7be5fb42699360dc758d723c316f13ef7
jianzhi Wang <wangj363@myumanitoba.ca>
2021-02-22 20:29:48 -0600

    Update Database.java

--- app/src/main/java/com/example/coursescheduler/Database/Database.java
+++ app/src/main/java/com/example/coursescheduler/Database/Database.java
@@ -1,19 +1,38 @@
 package com.example.coursescheduler.Database;
 
 public class Database {
-
-
-    public static String [][] studentInfo = { { "Simrandeep", "7784215" },
+    String [] timeArea ={
+            "08:30-09:30",
+            "10:30-11:20",
+            "11:20-12:30",
+            "13:30-14:20"
+    };
+    //10 test students Info
+    String [][] studentInfo = {
+            { "Simrandeep", "7784215" },
             { "Mohammad", "7853346" },
-            { "Rusty", "7850109"}};
+            { "Rusty", "7850109"},
+            {"Alan","7840761"},
+            {"Verne","7841354"},
+            {"Rudolf", "7854451"},
+            {"Norman","7843879"},
+            {"Shirley","7846612"},
+            {"Mamie","7841106"},
+            {"Lucy","7849975"},
+            {"Jill","7847416"}
+    };
+    //10 test course Info
+    String [][] courseInfo = {
+            { "Comp1010", "Intro Computer Science 1", timeArea[0], "TR" },
+            { "Comp2080", "Analysis of Algorithms", timeArea[1], "MWF" },
+            { "Comp2150", "Object Orientation", timeArea[1], "MWF"},
+            { "Comp3020", "Human-Computer Interaction 1", timeArea[2], "TR" },
+            { "Comp3040", "Technical Communication", timeArea[1], "TR" },
+            { "Comp3350", "Software Engineering 1", timeArea[2], "MWF"},
+            { "Comp4380", "Database Implementation", timeArea[0], "TR" },
+            { "Stat2000", "Basic Statistical Analysis 2", timeArea[3], "MWF"},
+            { "Econ2010", "MicroEconomics Theory 1", timeArea[1], "TR"},
+            { "Econ2020", "MacroEconomics Theory 1", timeArea[1], "TR"}
+    };
 
-    public static String [][] courseInfo = { { "Comp1010", "Intro Computer Science 1", "time1", "TR" },
-            { "Comp2080", "Analysis of Algorithms", "time2", "MWF" },
-            { "Comp2150", "Object Orientation", "time1", "MWF"},
-            { "Comp3020", "Human-Computer Interaction 1", "time3", "TR" },
-            { "Comp3040", "Technical Communication", "time2", "TR" },
-            { "Comp3350", "Software Engineering 1", "time3", "MWF"},
-            { "Comp4380", "Database Implementation", "time1", "TR" },
-            { "Stat2000", "Basic Statistical Analysis 2", "time4", "MWF"},
-            { "Econ2010", "MicroEconomics Theory 1", "time2", "TR"}};
 }
