cbcf5b7e77044771b557a928baa9a47515f60ad0
Guannan zhu <zhug34@myumanitoba.ca>
2021-02-23 13:19:12 -0600

    Replace ScheduleActivity.java

--- app/src/main/java/com/example/coursescheduler/myGIU/ScheduleActivity.java
+++ app/src/main/java/com/example/coursescheduler/myGIU/ScheduleActivity.java
@@ -2,7 +2,10 @@ package com.example.coursescheduler.myGIU;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.coursescheduler.R;
@@ -11,6 +14,9 @@ public class ScheduleActivity extends AppCompatActivity {
 
     String Name;
     TextView result;
+    TextView theFall;
+    TextView theWinter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +24,31 @@ public class ScheduleActivity extends AppCompatActivity {
         setContentView(R.layout.activity_schedule);
         result = (TextView)findViewById(R.id.displaySchedule);
         Name = getIntent().getExtras().getString("Name");
-        result.setText("Name: " + Name);
+        result.setText("Name:"+" " + Name);
+        theFall = (TextView)findViewById(R.id.fall);
+        theFall.setText("Fall: ");
+        theWinter = (TextView)findViewById(R.id.winter);
+        theWinter.setText("Winter: ");
+        Button add = (Button)findViewById(R.id.adding);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //this will change the page to courseActivity.java
+                Intent courseIntent = new Intent(ScheduleActivity.this, CourseActivity.class);
+                startActivity(courseIntent);
+            }
+        });
+
+        Button back = (Button)findViewById(R.id.Back);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //this will change the page to mainActivity.java
+                Intent mainIntent = new Intent(ScheduleActivity.this, MainActivity.class);
+                startActivity(mainIntent);
+            }
+        });
     }
 }
