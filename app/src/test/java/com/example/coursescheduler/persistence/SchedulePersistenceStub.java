120776323e9d4a4d48486219e1559d9154180fe3
Guannan zhu <zhug34@myumanitoba.ca>
2021-04-01 13:43:05 -0500

    Update SchedulePersistenceStub.java

--- app/src/test/java/com/example/coursescheduler/persistence/SchedulePersistenceStub.java
+++ app/src/test/java/com/example/coursescheduler/persistence/SchedulePersistenceStub.java
@@ -3,46 +3,37 @@ package com.example.coursescheduler.persistence;
 import android.database.sqlite.SQLiteDatabase;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import com.example.coursescheduler.objects.Schedule;
 import com.example.coursescheduler.objects.Student;
 
 
-public class SchedulePersistenceStub implements ISchedule{
+public class SchedulePersistenceStub implements ISchedule {
     private List<Schedule> schedule;
 
     public SchedulePersistenceStub() {
         this.schedule = new ArrayList<>();
 
         schedule.add(new Schedule(7834177,3010));
-        schedule.add(new Schedule(7834567,3020));
-        schedule.add(new Schedule(7845678,3350));
-        schedule.add(new Schedule(7812345,3380));
+        schedule.add(new Schedule( 7834567,3020));
+        schedule.add(new Schedule( 7845678,3350));
+        schedule.add(new Schedule( 7812345,3380));
     }
-///    @Override
-//    public void onCreate(SQLiteDatabase db) {
-//    }
-
     @Override
-    public List<Schedule> getSequential(Student object) {
-        return null;
+    public List<Schedule> getSequential(Student currentstudent) {
+        List<Schedule> newSchedule = null;
+        for(int i = 0; i<schedule.size(); i++){
+            if (schedule.get(i).getStudentID()==currentstudent.getStudentID())
+                newSchedule.add(schedule.get(i));
+        }
+
+        return newSchedule;
     }
 
     @Override
     public void insert(Schedule currentSchedule) {
         // don't bother checking for duplicates
         schedule.add(currentSchedule);
-    }
-
-    @Override
-    public boolean deleteSchedule(Student deleteObject) {
-        return false;
-    }
-
-    @Override
-    public boolean deleteCourse(Schedule deleteObject) {
-        return false;
     }
 
     @Override
@@ -59,10 +50,20 @@ public class SchedulePersistenceStub implements ISchedule{
     }
 
     @Override
-    public boolean delete(Schedule currentCourse) {
+    public boolean deleteSchedule(Student deleteObject){
+        for(int i = 0; i<schedule.size(); i++){
+            if (schedule.get(i).getStudentID()==deleteObject.getStudentID())
+                schedule.remove(i);
+                return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean deleteCourse(Schedule deleteObject) {
         int index;
 
-        index = schedule.indexOf(currentCourse);
+        index = schedule.indexOf(deleteObject);
         if (index >= 0)
         {
             schedule.remove(index);
@@ -70,11 +71,11 @@ public class SchedulePersistenceStub implements ISchedule{
         }
         return false;
     }
-    public Schedule fetch(Schedule currentCourse) {
+    public Schedule fetch(Schedule currentSchedule) {
         //check if the parameter is in the list of course and then return the course object
         for(int i=0; i<schedule.size();i++) {
-            if (currentCourse.equals(schedule.get(i))) {
-                return currentCourse;
+            if (currentSchedule.equals(schedule.get(i))) {
+                return currentSchedule;
             }
         }
         return null;
