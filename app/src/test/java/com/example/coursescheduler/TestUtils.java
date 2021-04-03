8c47f07dddd4ea671d9daf7cae3f1aa901fdd656
jianzhiWang10086 <74884772+jianzhiWang10086@users.noreply.github.com>
2021-04-03 16:42:04 -0500

    Start IT course test

--- app/src/test/java/com/example/coursescheduler/TestUtils.java
+++ app/src/test/java/com/example/coursescheduler/TestUtils.java
@@ -4,13 +4,13 @@ import com.google.common.io.Files;
 
 import java.io.File;
 import java.io.IOException;
-
+import com.example.coursescheduler.application.Services;
 public class TestUtils {
     private static final File DB_SRC = new File("src/main/assets/db/SC.script");
     public static File copyDB() throws IOException {
         final File target = File.createTempFile("temp-db", ".script");
         Files.copy(DB_SRC, target);
-        Main.setDBPathName(target.getAbsolutePath().replace(".script", ""));
+        //Services.setDBPathName(target.getAbsolutePath().replace(".script", ""));
         return target;
     }
 }
--- app/src/test/java/com/example/coursescheduler/business/AccessCourseIT.java
+++ app/src/test/java/com/example/coursescheduler/business/AccessCourseIT.java
@@ -1,5 +1,50 @@
 package com.example.coursescheduler.business;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import android.content.Context;
+import android.util.Log;
+
+import com.example.coursescheduler.application.Services;
+import com.example.coursescheduler.objects.Course;
+import com.example.coursescheduler.persistence.IDatabase;
+import com.example.coursescheduler.business.AccessCourse;
+import com.example.coursescheduler.persistence.sqlite_db.CoursePersistence;
+
+import java.util.Collections;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 
 public class AccessCourseIT {
+    private AccessCourse accessCourse;
+    private File tempDB;
+
+    @Before
+    public void setUp() throws IOException {
+        //this.tempDB = ;
+        //final CoursePersistence coursePersistence = new CoursePersistence(this.tempDB.getAbsolutePath().replace(".script", ""));
+
+        //this.accessCourse = new AccessCourse(coursePersistence);
+    }
+    @Test
+    public void testListCourses() {
+        final List<Course> courses;
+        final Course course;
+        courses = accessCourse.getCourseSequential();
+        course = courses.get(0);
+        assertNotNull("first sequential course should not be null", course);
+        assertTrue("COMP3010".equals(course.getCourseId()));
+        System.out.println("Finished test AccessCourses");
+    }
+
+
+
 
 }
