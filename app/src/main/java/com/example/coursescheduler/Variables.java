cc181a3e0f563b379f8d698432998e8ef0cb3be8
Simrandeep <simrandeep.sappal@gmail.com>
2021-04-03 18:05:08 -0500

    Created a variables file to contain frequently used variables

---
+++ app/src/main/java/com/example/coursescheduler/Variables.java
@@ -0,0 +1,17 @@
+package com.example.coursescheduler;
+
+public class Variables {
+    public static String student_ID = "studentID";
+    public static String student_Name = "studentName";
+    public static String course_ID = "courseID";
+
+    public static String name = "Name";
+    public static String id = "ID";
+    public static String time = "Time";
+    public static String day = "Day";
+
+    public static String tag = "myTag";
+    public static String error = "Error";
+
+
+}
